1093d7e542d9cd426cfb7708ba143625b7a83893
Eric J Fisher <EricJFisher@users.noreply.github.com>
2020-10-27 03:36:22 -0400

    Added containers for Life and Energy bars. (these will be refactored later)

--- End of Heroes/Desktop.cs
+++ End of Heroes/Desktop.cs
@@ -9,42 +9,67 @@ namespace EndOfHeroes
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        public Texture2D Texture;
+        public Vector2 Position;
+
+        public Texture2D HudTexture;
+        public Vector2 HudPosition;
+
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+
         public Desktop()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-            IsMouseVisible = true;
+            Position = new Vector2(0, 0);
+
+            ScreenWidth = _graphics.PreferredBackBufferWidth;
+            ScreenHeight = _graphics.PreferredBackBufferHeight;
+
+            HudPosition = new Vector2(ScreenWidth/2-64,ScreenHeight-128);
         }
 
         protected override void Initialize()
         {
-            // TODO: Add your initialization logic here
-
             base.Initialize();
+            Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width /2,
+                _graphics.GraphicsDevice.Viewport.Height /2);
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            Texture = Content.Load<Texture2D>("placeholder");
+            HudTexture = Content.Load<Texture2D>("vitals");
         }
 
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (state.IsKeyDown(Keys.Right))
+                Position.X += 10;
+            if (state.IsKeyDown(Keys.Left))
+                Position.X -= 10;
+            if (state.IsKeyDown(Keys.Up))
+                Position.Y -= 10;
+            if (state.IsKeyDown(Keys.Down))
+                Position.Y += 10;
 
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.Clear(Color.Black);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(Texture, Position, Color.White);
+            _spriteBatch.Draw(HudTexture, HudPosition, Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
