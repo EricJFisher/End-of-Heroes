3f03b4a01157108d0cd9c4d5080ae7f52b7df68f
Eric J Fisher <EricJFisher@users.noreply.github.com>
2020-10-28 01:47:22 -0400

    Refactored game code into it's own project (Desktop project will house only code exclusively relevant to the desktop client)

---
+++ DystopiaEngine/Engine.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace DystopiaEngine
+{
+    public class Engine : Game
+    {
+        public static GraphicsDeviceManager Graphics;
+        public static SpriteBatch SpriteBatch;
+
+        public Texture2D Texture;
+        public Vector2 Position;
+
+        public Texture2D HudTexture;
+        public Vector2 HudPosition;
+
+        public static int ScreenWidth;
+        public static int ScreenHeight;
+
+        public Engine()
+        {
+            Graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+
+
+            ScreenWidth = Graphics.PreferredBackBufferWidth;
+            ScreenHeight = Graphics.PreferredBackBufferHeight;
+
+            Position = new Vector2(0, 0);
+            HudPosition = new Vector2(ScreenWidth / 2 - 64, ScreenHeight - 128);
+        }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            SpriteBatch = new SpriteBatch(GraphicsDevice);
+            Texture = Content.Load<Texture2D>("placeholder");
+            HudTexture = Content.Load<Texture2D>("vitals");
+
+            base.LoadContent();
+        }
+
+        protected override void UnloadContent()
+        {
+            base.UnloadContent();
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState state = Keyboard.GetState();
+            if (state.IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (state.IsKeyDown(Keys.Right))
+                Position.X += 10;
+            if (state.IsKeyDown(Keys.Left))
+                Position.X -= 10;
+            if (state.IsKeyDown(Keys.Up))
+                Position.Y -= 10;
+            if (state.IsKeyDown(Keys.Down))
+                Position.Y += 10;
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.Black);
+
+            SpriteBatch.Begin();
+            SpriteBatch.Draw(Texture, Position, Color.White);
+            SpriteBatch.Draw(HudTexture, HudPosition, Color.White);
+            SpriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
--- End of Heroes/Desktop.cs
+++ End of Heroes/Desktop.cs
@@ -1,77 +1,12 @@
-﻿using Microsoft.Xna.Framework;
+﻿using DystopiaEngine;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
 namespace EndOfHeroes
 {
-    public class Desktop : Game
+    public class Desktop : Engine
     {
-        private GraphicsDeviceManager _graphics;
-        private SpriteBatch _spriteBatch;
-
-        public Texture2D Texture;
-        public Vector2 Position;
-
-        public Texture2D HudTexture;
-        public Vector2 HudPosition;
-
-        public static int ScreenWidth;
-        public static int ScreenHeight;
-
-        public Desktop()
-        {
-            _graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-            Position = new Vector2(0, 0);
-
-            ScreenWidth = _graphics.PreferredBackBufferWidth;
-            ScreenHeight = _graphics.PreferredBackBufferHeight;
-
-            HudPosition = new Vector2(ScreenWidth/2-64,ScreenHeight-128);
-        }
-
-        protected override void Initialize()
-        {
-            base.Initialize();
-            Position = new Vector2(_graphics.GraphicsDevice.Viewport.Width /2,
-                _graphics.GraphicsDevice.Viewport.Height /2);
-        }
-
-        protected override void LoadContent()
-        {
-            _spriteBatch = new SpriteBatch(GraphicsDevice);
-            Texture = Content.Load<Texture2D>("placeholder");
-            HudTexture = Content.Load<Texture2D>("vitals");
-        }
-
-        protected override void Update(GameTime gameTime)
-        {
-            KeyboardState state = Keyboard.GetState();
-            if (state.IsKeyDown(Keys.Escape))
-                Exit();
-
-            if (state.IsKeyDown(Keys.Right))
-                Position.X += 10;
-            if (state.IsKeyDown(Keys.Left))
-                Position.X -= 10;
-            if (state.IsKeyDown(Keys.Up))
-                Position.Y -= 10;
-            if (state.IsKeyDown(Keys.Down))
-                Position.Y += 10;
-
-            base.Update(gameTime);
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.Black);
-
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(Texture, Position, Color.White);
-            _spriteBatch.Draw(HudTexture, HudPosition, Color.White);
-            _spriteBatch.End();
-
-            base.Draw(gameTime);
-        }
+        // Code only relevent to the desktop client itself
     }
 }
