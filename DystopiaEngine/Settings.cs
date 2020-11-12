ac39b3b8a8c936aa97df7e810570489f3fc5f083
Eric J Fisher <EricJFisher@users.noreply.github.com>
2020-11-11 22:23:22 -0500

    Updated to .NET 5! (That was suprisingly painless for monogame)

---
+++ DystopiaEngine/Settings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Text.Json;
+
+namespace DystopiaEngine
+{
+    public class Settings
+    {
+        public static bool IsFullscreen { get; set; }
+        public static int WindowWidth { get; set; }
+        public static int WindowHeight { get; set; }
+
+        // Windows only settings, to support OSX and Linux this will need adapted to set the directory accordingly
+        private readonly string _path = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+        public Settings()
+        {
+            IsFullscreen = false;
+            WindowWidth = 720;
+            WindowHeight = 1280;
+        }
+
+        public void Load()
+        {
+            var json = File.ReadAllText(_path);
+            var settings = JsonSerializer.Deserialize<Settings>(json);
+        }
+
+        public void Save()
+        {
+            var json = JsonSerializer.Serialize(this);
+            File.WriteAllText(_path, json);
+        }
+    }
+}
